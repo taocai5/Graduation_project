a0513e39be07966a3355c0a3b0f10081e38a0c03
gugu <gudaya04@foxmail.com>
2026-04-13 20:00:10 +0800

    重构项目结构&&更换数据集为特征工程准备

--- run.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-if not exist "output" mkdir output
-g++ src/main.cpp src/csv_reader.cpp src/kmeans.cpp -o main.exe
-if %errorlevel% neq 0 (
-    echo Compilation failed.
-    exit /b %errorlevel%
-)
-main.exe
-   
